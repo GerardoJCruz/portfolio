855b2f6db468621244df2bcb4fd4c52a814cd3f7
Gerardo J Cruz <gerardojcruz98@gmail.com>
2023-06-22 19:04:44 -0600

    Portfolio Gerardo

--- src/screens/Header.js
+++ src/screens/Header.js
@@ -1,18 +1,13 @@
 import React from 'react'
-import Typed from 'react-typed'
 
 
 function Header() {
     return (
         <div className='header-info'>
             <h1 className='header-title'> Who am I?</h1>
-            <Typed
-                    strings={[
-                        'Junior web developer ready to fuel business growth', 'through innovation', '']}
-                        typeSpeed={50}
-                        backSpeed={50}
-                        loop 
-                />
+            <p>
+              I am a front-end web developer ready to fuel business growth through innovation
+            </p>
         </div>
     )
 }
