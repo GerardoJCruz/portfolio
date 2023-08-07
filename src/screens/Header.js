6224419772d5a2a1c84de274bee382c2095c332e
Gerardo J Cruz <gerardojcruz98@gmail.com>
2023-08-06 21:31:05 -0600

    Background image performance correction

--- src/screens/Header.js
+++ src/screens/Header.js
@@ -1,17 +1,21 @@
-import React from 'react'
-
+import React from 'react';
+import backgroundImage from '../images/utah-bg.jpg'; // Make sure to provide the correct path to the background image
 
 function Header() {
     return (
-        <div className='header-bg'>
-        <div className='header-info'>
-            <h1 className='header-title'> Who am I?</h1>
-            <p>
-                I am a front-end web developer ready to fuel business growth through innovation
-            </p>
+        <div className='header'>
+            <div className='header-bg' style={{ backgroundImage: `url(${backgroundImage})` }}>
+                {/* Add the pseudo-element with the gradient */}
+                <div className="gradient-overlay"></div>
+            </div>
+            <div className='header-info'>
+                <h1 className='header-title'> Who am I?</h1>
+                <p>
+                    I am a front-end web developer ready to fuel business growth through innovation
+                </p>
+            </div>
         </div>
-        </div>
-    )
+    );
 }
 
-export default Header
+export default Header;
