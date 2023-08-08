bb546053c6f6ace9b9e5b5d04bc93a8e3ef4e81b
Gerardo J Cruz <gerardojcruz98@gmail.com>
2023-08-08 17:02:57 -0600

    bg-img

--- src/screens/Header.js
+++ src/screens/Header.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import backgroundImage from '../images/utah-bg.jpg'; // Make sure to provide the correct path to the background image
 
 function Header() {
+    useEffect(()=>{
+        //Preload the background image
+        const img = new Image();
+        img.src = backgroundImage;
+    }, []);
+
     return (
         <div className='header'>
-            <div className='header-bg' style={{ backgroundImage: `url(${backgroundImage})` }}>
-                {/* Add the pseudo-element with the gradient */}
+            <div className='header-bg' style={{ backgroundImage: `url(${backgroundImage})`}} loading="lazy">
+                {/* Add a pseudo-element with the gradient to create a filter*/}
                 <div className="gradient-overlay"></div>
             </div>
             <div className='header-info'>
